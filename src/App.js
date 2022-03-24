b66dabc26d9e3ca1c909fbf1a8a1508a8aaca94d
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-03-24 23:23:03 +0900

    about page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./page/about";
+import Profile from "./page/profile";
+
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="about" element={<About />} />
+        <Route path="profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
